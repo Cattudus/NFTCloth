51a00f28c112ed3e99114aa62025604b5cf068bf
cattu <cattuduslol@gmail.com>
2021-10-17 13:06:53 +0200

    Updates

---
+++ _next/static/V4mO3qjd0K_A6P8XGHPC5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-e484eb0f7de62d1c9b9d.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
