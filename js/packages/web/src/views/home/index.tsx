cac597a2ea69591379edfac48f0cba9988964f44
cattu <cattuduslol@gmail.com>
2021-10-27 12:10:17 +0200

    feat: Button to  go up

--- js/packages/web/src/views/home/index.tsx
+++ js/packages/web/src/views/home/index.tsx
@@ -12,6 +12,7 @@ import { LayoutContent } from '../../components/Layout';
 import { HauntedVillage } from '../../components/HauntedVillage';
 import { SpectralShopSection } from '../../components/SpectralShopSection';
 import { LandmarksSection } from '../../components/LandmarksSection';
+import {UpOutlined} from "@ant-design/icons";
 
 export const HomeView = () => {
   // const { isLoading, store } = useMeta();
@@ -27,6 +28,26 @@ export const HomeView = () => {
   return <MainView />;
 };
 export const MainView = () => {
+  let mybutton = document.getElementById("myBtn");
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      if(mybutton != null) {
+        mybutton.style.display = "block";
+      }
+    } else {
+      if(mybutton != null){
+      mybutton.style.display = "none";
+      }
+    }
+    mybutton = document.getElementById("myBtn");
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
   return (
     <div>
       <AboutUs />
@@ -40,6 +61,7 @@ export const MainView = () => {
       <Creators />
       <FAQ />
       <Footer />
+      <button onClick={topFunction} id="myBtn" title="Go to top"><UpOutlined /></button>
     </div>
   );
 };
