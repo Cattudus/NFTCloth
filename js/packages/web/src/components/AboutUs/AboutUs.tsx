dde764bdedcb00826cc694be10b9795658b99fbf
cattu <cattuduslol@gmail.com>
2021-10-27 15:06:01 +0200

    refactor: About us change

--- js/packages/web/src/components/AboutUs/AboutUs.tsx
+++ js/packages/web/src/components/AboutUs/AboutUs.tsx
@@ -71,7 +71,7 @@ export const AboutUs: FC = () => {
   const [picture, setPicture] = useState(1);
   const [nft, setNft] = useState('/NFTs/' + picture + '.png');
   return (
-    <div style={{ paddingTop: '85px' }}>
+    <div style={{ paddingTop: '50px' }}>
       <LayoutContent>
         <div>
           <div className="about-us">
